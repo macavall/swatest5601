22b91870d0f472808a3c5d9ffccb55890bd3bac7
Matt <macavall@microsoft.com>
2025-05-13 18:09:25 -0500

    first

--- api/http1.cs
+++ api/http1.cs
@@ -10,7 +10,7 @@ namespace swatest5601fa;
 public class http1
 {
     private readonly ILogger<http1> _logger;
-    private int counter = 0;
+    // private int counter = 0;
 
     public http1(ILogger<http1> logger)
     {
@@ -22,28 +22,28 @@ public class http1
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-        if (counter != 1)
-        {
-            counter = 1;
+        // if (counter != 1)
+        // {
+        //     counter = 1;
 
-            _ = Task.Factory.StartNew(async () =>
-            {
-                while(true)
-                {
-                    _logger.LogInformation($"Counter: {counter}");
+        //     _ = Task.Factory.StartNew(async () =>
+        //     {
+        //         while(true)
+        //         {
+        //             _logger.LogInformation($"Counter: {counter}");
                     
-                    using(var httpClient = new HttpClient())
-                    {
-                        var httpReq = new HttpRequestMessage(HttpMethod.Get, "https://ambitious-field-0ee1c3f0f.6.azurestaticapps.net");
+        //             using(var httpClient = new HttpClient())
+        //             {
+        //                 var httpReq = new HttpRequestMessage(HttpMethod.Get, "https://ambitious-field-0ee1c3f0f.6.azurestaticapps.net");
 
-                        var response = await httpClient.SendAsync(httpReq);
-                        _logger.LogInformation($"Response Code: {response.StatusCode}");
-                    }
+        //                 var response = await httpClient.SendAsync(httpReq);
+        //                 _logger.LogInformation($"Response Code: {response.StatusCode}");
+        //             }
 
-                    Thread.Sleep(6000);
-                }
-            });
-        }        
+        //             Thread.Sleep(6000);
+        //         }
+        //     });
+        // }        
 
         return new OkObjectResult("Welcome to Azure Functions!");
     }
